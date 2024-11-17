c4c830fa7072f6c4b8b70e33fcecf1918a1db2df
suriya-shanmugam <suriya.shanmugam.us@gmail.com>
2024-11-16 17:56:59 -0800

    Initial commit: Setup simple Express.js app

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
